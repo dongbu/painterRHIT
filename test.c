161ccdf7ea7dd60566365a42f2fb73f0586457d0
System Administrator <root@DREWMAC.local>
2014-09-24 16:38:25 -0700

    simplify compiling

--- test.c
+++ test.c
@@ -1,8 +1,22 @@
-// my first program in C++
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main()
+int main( int argc, char** argv )
 {
+
+  char* name = argv[1];
+  if (!name) {
+    std::cout << "!!! arg: <filename.cpp>" << std::endl;
+    return -1;
+  }  
+
+  char cmd[500];
+  sprintf(cmd,"Lines: drew %i out of %lu",num_drawn,contours.size());
+
+
+g++ contours.cpp -o contours  `pkg-config --cflags --libs opencv` -Wall
+
   std::cout << "Hello World!\n";
 }
 
