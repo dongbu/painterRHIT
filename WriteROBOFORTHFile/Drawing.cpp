80611bae71a9d04a050fbaa38108ddfdfcdf8cb5
seanakl <klingsa@KLINGSA-1.rose-hulman.edu>
2014-11-01 22:20:11 -0400

    modified ROBOFORTH command writing

--- WriteROBOFORTHFile/Drawing.cpp
+++ WriteROBOFORTHFile/Drawing.cpp
@@ -10,6 +10,10 @@ using namespace cv;
 
 void writeROBOFORTHFromContours(ofstream &myfile, vector<vector<Point> > &contours, double z, double scale){
 	char movetoCommand[25];
+	sprintf(movetoCommand, "10000 SPEED !\n");
+	myfile << movetoCommand;
+	sprintf(movetoCommand, "1000 ACCEL !\n");
+	myfile << movetoCommand;
 
 	for (vector<vector<Point> >::iterator pathIterator = contours.begin(); pathIterator != contours.end(); pathIterator++) {
 		for (vector<Point>::iterator pointIterator = pathIterator->begin(); pointIterator != pathIterator->end(); pointIterator++){
@@ -30,39 +34,75 @@ void writeROBOFORTHFromContours(ofstream &myfile, vector<vector<Point> > &contou
 
 void stopBrush(ofstream &myfile, Brush &currentBrush){
 	char movetoCommand[25];
+	sprintf(movetoCommand, "1000 SPEED !\n");
+	myfile << movetoCommand;
+	sprintf(movetoCommand, "100 ACCEL !\n");
+	myfile << movetoCommand;
+
+	double plungeDepth = 100.0;
+	double increment = 5.0;
+
 	// Retract from drawing
 	myfile << "READY\n";
 	myfile << "0.0 0.0 100.0 MOVE\n";
 
 	// Move to a position to put back the current brush
-	sprintf(movetoCommand, "%.1f %.1f %.1f MOVETO\n", currentBrush.x, currentBrush.y, currentBrush.z + 100.0f);
-	myfile << movetoCommand;
-	sprintf(movetoCommand, "%.1f %.1f %.1f MOVETO\n", currentBrush.x, currentBrush.y, currentBrush.z);
+	sprintf(movetoCommand, "%.1f %.1f %.1f MOVETO\n", currentBrush.x, currentBrush.y, currentBrush.z + plungeDepth);
 	myfile << movetoCommand;
 
+	// Plunge downwards
+	plunge(myfile, plungeDepth, increment);
+
 	// Let go of the brush
-//	myfile << "UNGRIP\n";
 	sprintf(movetoCommand, "TELL WRIST -500 MOVE\n");
 	myfile << movetoCommand;
 }
 
 void startBrush(ofstream &myfile, Brush &nextBrush){
 	char movetoCommand[25];
-	// Retract
-	myfile << "0.0 0.0 100.0 MOVE\n";
+	sprintf(movetoCommand, "1000 SPEED !\n");
+	myfile << movetoCommand;
+	sprintf(movetoCommand, "100 ACCEL !\n");
+	myfile << movetoCommand;
 
-	// Move to a position to put back the current brush
-	sprintf(movetoCommand, "%.1f %.1f %.1f MOVETO\n", nextBrush.x, nextBrush.y, nextBrush.z + 100.0f);
+
+	double plungeDepth = 100.0;
+	double increment = 5.0;
+
+	// Retract from previous brush
+	retract(myfile, plungeDepth, increment);
+
+	// Move to a position to pick up the next brush
+	sprintf(movetoCommand, "%.1f %.1f %.1f MOVETO\n", nextBrush.x, nextBrush.y, nextBrush.z + plungeDepth);
 	myfile << movetoCommand;
-	sprintf(movetoCommand, "%.1f %.1f %.1f MOVETO\n", nextBrush.x, nextBrush.y, nextBrush.z);
-	myfile << movetoCommand;
+
+	// Plunge downwards
+	plunge(myfile, plungeDepth, increment);
 
 	// Grab the next brush
-//	myfile << "GRIP\n";
 	sprintf(movetoCommand, "TELL WRIST 500 MOVE\n");
 	myfile << movetoCommand;
 
-	// Retract
-	sprintf(movetoCommand, "%.1f %.1f %.1f MOVETO\n", nextBrush.x, nextBrush.y, nextBrush.z + 100.0f);
-	myfile << movetoCommand;
+	// Retract upwards
+	retract(myfile, plungeDepth, increment);
+}
+
+// Retract upwards
+void retract(ofstream &myfile, double plungeDepth, double increment){
+	char movetoCommand[25];
+
+	for(int i = 0; i < plungeDepth; i = i + increment){
+		sprintf(movetoCommand, "%.1f %.1f %.1f MOVE\n", 0.0f, 0.0f, increment);
+		myfile << movetoCommand;
+	}
+}
+
+// Plunge downwards
+void plunge(ofstream &myfile, double plungeDepth, double increment){
+	char movetoCommand[25];
+
+	for(int i = 0; i < plungeDepth; i = i + increment){
+		sprintf(movetoCommand, "%.1f %.1f %.1f MOVE\n", 0.0f, 0.0f, -increment);
+		myfile << movetoCommand;
+	}
 }
--- WriteROBOFORTHFile/Drawing.hpp
+++ WriteROBOFORTHFile/Drawing.hpp
@@ -13,4 +13,6 @@ using namespace cv;
 void writeROBOFORTHFromContours(ofstream &myfile, vector<vector<Point> > &contours, double z, double scale = 1);
 void stopBrush(ofstream &myfile, Brush &currentBrush);
 void startBrush(ofstream &myfile, Brush &nextBrush);
+void retract(ofstream &myfile, double plungeDepth, double increment);
+void plunge(ofstream &myfile, double plungeDepth, double increment);
 #endif
