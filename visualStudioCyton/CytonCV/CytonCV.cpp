cdeee2591305681cc7ac6d566fc9df4851854aa1
seanakl <klingsa@KLINGSA-W520.rose-hulman.edu>
2015-05-07 10:59:01 -0400

    work from wednesday

--- visualStudioCyton/CytonCV/CytonCV.cpp
+++ visualStudioCyton/CytonCV/CytonCV.cpp
@@ -3,6 +3,7 @@
 #include <String>
 #include <opencv2/opencv.hpp>
 #include <boost/thread/tss.hpp>
+#include "ImageToStroke.h"
 
 #include <control/ecEndEffectorSet.h>
 #include <control/ecFrameEndEffector.h>
@@ -16,17 +17,18 @@
 #include <manipulationDirector/ecManipulationScript.h>
 #include <remoteCommand/ecRemoteCommand.h>
 
-# define M_PI 3.14159265358979323846  /* pi */
+# define M_PI 3.141592653  /* pi */
 
 #define POINT_EE_SET 0
 #define FRAME_EE_SET 1
 #define JOINT_CONTROL_EE_SET 0xFFFFFFFF
 #define COLORS 4
+#define SOURCE_COLOR0	Point3d(0.2586, -0.0693, 0.0682)
 #define SOURCE_COLOR1	Point3d(0.2586, -0.0693, 0.0682)
 #define SOURCE_COLOR2	Point3d(0.1938, -0.0579, 0.0682)
-#define SOURCE_COLOR3	Point3d(0.1938, -0.0579, 0.0682)
-#define DROPOFF		Point3d(0.2586, -0.0693, 0.0682)
-#define PICKUP		Point3d(0.1938, -0.0579, 0.0682)
+#define SOURCE_COLOR3	Point3d(0.0986, -0.0566, 0.0682)
+#define DROPOFF		Point3d(0.2, -0.2, 0.1)
+#define PICKUP		Point3d(0.1, -0.1, 0.1)
 
 using namespace cv;
 using namespace std;
@@ -37,11 +39,17 @@ void swap_brush(Point3d, Point3d);
 void get_paint(Point3d paintSource);
 void stroke();
 bool moveGripper(const double gripperPos);
-vector<Point3d> makeCircle(float z);
+vector<Point3d> makeCircle(double z);
 Point3d getLeadIn(vector<Point3d> contour);
 
 int main(int argc, char* argv[])
 {
+	VideoCapture stream1(0);
+	stream1.set(CV_CAP_PROP_FRAME_WIDTH, 400);
+	stream1.set(CV_CAP_PROP_FRAME_HEIGHT, 300);
+	if (!stream1.isOpened()) { //check if video device has been initialised
+		cout << "cannot open camera 1";
+	}
 	string inputFilePath = "C:\\Pictures\\logo.bmp";
 
 	int cannyThresh = 240;
@@ -50,77 +58,112 @@ int main(int argc, char* argv[])
 		inputFilePath = argv[1];
 	}
 
-	const Point3d offset = Point3d(0.1, -0.2, 0.1);
-	const double scale = 0.0006;
+	const Point3d offset = Point3d(0.0468, -0.3137, 0.0682);
+	const double scale = 0.6;
 	const Point3d liftOff = Point3d(0, 0, 0.02);
 
 	const Point3d Brush1Position;
 	const Point3d Brush2Position;
 
-	Mat greyscaleImage = imread(inputFilePath);
-	assert(greyscaleImage.data != NULL);
+	Mat image;
+	//image = imread(inputFilePath);
+	while (true) {
+		stream1.read(image);
+		imshow("cam", image);
+		if (waitKey(30) >= 0)
+			break;
+	}
+	stream1.release();
+
+	assert(image.data != NULL);
+	imshow("Image", image);
+	waitKey(0);
 
 	//imshow("Input Image", greyscaleImage);
-	Mat cannyOutput;
-	Canny(greyscaleImage, cannyOutput, cannyThresh, cannyThresh * 2);
-	assert(cannyOutput.data != NULL);
+	//Mat cannyOutput;
+	/*Canny(image, cannyOutput, cannyThresh, cannyThresh * 2);
+	assert(cannyOutput.data != NULL);*/
 	//imshow("Canny", cannyOutput);
 
-	vector<vector<Point>> contours;
-	findContours(cannyOutput, contours, RETR_EXTERNAL, CHAIN_APPROX_TC89_L1);
+	//vector<vector<Point>> contours;
+	//findContours(cannyOutput, contours, RETR_EXTERNAL, CHAIN_APPROX_TC89_L1);
 
 	//vector<pair<vector<Point>, int>> newFormat1; 
 	//newFormat[0].first = the path
 	//newFormat[0].second = the color index
 
+	vector<pair<vector<Point>, int> > contours = imageToStroke(image);
+
 	//code here to convert to
 	vector<pair<vector<Point3d>, int>> newFormat;
 	int colorIndex = 0;
-	for (vector<Point> i : contours) {
+	for (pair<vector<Point>, int > i : contours) {
 		vector<Point3d> tmpPath;
-		newFormat.push_back(make_pair(tmpPath, colorIndex));
-		colorIndex += 1;
-		colorIndex %= 4;
 		//cout << "new path" << endl;
-		for (Point j : i) {
+		for (Point j : i.first) {
 			Point3d k = Point3d(j.x, j.y, 0);
 			k = k*scale;
 			k = k + offset;
 			tmpPath.push_back(k);
-			//cout << k << endl;
 		}
+		newFormat.push_back(make_pair(tmpPath, i.second));
 	}
-	
 	if (init()) {
 		cout << "Established Connection" << endl;
 	}else {
 		cout << "Error establishing connection" << endl;
 		exit(1);
 	}
-	
+	vector<Point3d> circle;
+	double cm = 0.01;
+	cout << "Drawing circle" << endl;
+	for (double i = 0; i < 2 * M_PI; i += M_PI / 4){
+		circle.push_back(Point3d(cm*cos(i), cm*sin(i), 0));
+	}
 	Point3d leadin, lastPoint, source;
-	vector<Point3d> circle = makeCircle(newFormat[0].first[0].z);
+	double dx, dy, dist, lx, ly;
+	//vector<Point3d> circle = makeCircle(newFormat[0].first[0].z);
+	waitKey(0);
+	cout << newFormat.size() << endl;
+	for (pair<vector<Point3d>, int> i : newFormat){
+		cout << i.second << ", ";
+	}
+	cout << endl;
+
 	cout << "Moving through points" << endl;
-	for (int i = 1; i < COLORS; i++) {
+	for (int i = 0; i < COLORS; i++) {
 		for (unsigned int j = 0; j < newFormat.size(); j++) {
-			if (newFormat[j].second == i) {
-				// Get more paint				
-				if (i == 1){source = SOURCE_COLOR1;}
+			if (newFormat.at(j).second == i) {
+				// Get more paint
+				if (i == 0){source = SOURCE_COLOR0;}
+				else if (i == 1){source = SOURCE_COLOR1;}
 				else if (i == 2){source = SOURCE_COLOR2;}
 				else if (i == 3){source = SOURCE_COLOR3;}
 
-				net_move(source + Point3d(0, 0, 0.02));
+				net_move(source + Point3d(0, 0, 0.1));
 				net_move(source);
 				// Move in a circle
-				for (int n = 0; n < circle.size(); n++){
-					net_move(source + circle[n]);
+				for (unsigned int n = 0; n < circle.size(); n++){
+					net_move(source + circle.at(n));
 				}
-				net_move(source + Point3d(0, 0, 0.02));
+				net_move(source + Point3d(0, 0, 0.1));
 
 				// Lead-in motion
-				net_move(getLeadIn(newFormat[j].first));
+				if (newFormat.at(j).first.size() < 2){
+					leadin = newFormat.at(j).first.at(0);
+				}
+				else{
+					dy = (newFormat.at(j).first.at(1).y - newFormat.at(j).first.at(0).y);
+					dx = (newFormat.at(j).first.at(1).x - newFormat.at(j).first.at(0).x);
+					dist = sqrt(dx*dx + dy*dy);
+					lx = newFormat.at(j).first.at(0).x - 0.02 * (dx / dist);
+					ly = newFormat.at(j).first.at(0).y - 0.02 * (dy / dist);
+					leadin = Point3d(lx, ly, (newFormat.at(j).first.at(0).z + 0.01));
+				}
+				net_move(leadin);
+				//net_move(getLeadIn(newFormat[j].first));
 				
-				for (Point3d k : newFormat[j].first) {
+				for (Point3d k : newFormat.at(j).first) {
 					net_move(k);
 					lastPoint = k;
 				}
@@ -138,22 +181,25 @@ int main(int argc, char* argv[])
 }
 
 Point3d getLeadIn(vector<Point3d> contour){
+	if (contour.size() < 2){
+		return contour[0];
+	}
 	Point3d leadin;
-	int dx, dy, dist, lx, ly;
+	double dx, dy, dist, lx, ly;
 	dy = (contour[1].y - contour[0].y);
 	dx = (contour[1].x - contour[0].x);
 	dist = sqrt(dx*dx + dy*dy);
 	lx = contour[0].x - 0.02 * (dx / dist);
 	ly = contour[0].y - 0.02 * (dy / dist);
-	leadin = Point3d(lx, ly, (contour[0].z + 0.01));
+	leadin = Point3d(lx, ly, (contour[0].z + 0.02));
 	return leadin;
 }
 
-vector<Point3d> makeCircle(float z){
+vector<Point3d> makeCircle(double z){
 	vector<Point3d> circle;
-	float cm = 0.01;
-	for (float i = 0; i < 2*M_PI; i += M_PI/4){
-		circle[i] = Point3d(cm*cos(i), cm*sin(i), z);
+	double cm = 0.01;
+	for (double i = 0; i < 2*M_PI; i += M_PI/4){
+		circle.push_back(Point3d(cm*cos(i), cm*sin(i), z));
 	}
 	return circle;
 }
@@ -165,12 +211,15 @@ void get_paint(Point3d paintSource){
 }
 
 void swap_brush(Point3d brushDropoff, Point3d newBrush) {
+	cout << "Swapping brush" << endl;
+	net_move(Point3d(0.25, 0, 0.2));
+	net_move(Point3d(0.25, 0.1, 0.2));
 	net_move(brushDropoff + Point3d(0, 0, 0.1));
 	net_move(brushDropoff);
 	moveGripper(0.002);
 	net_move(brushDropoff + Point3d(0, 0, 0.1));
 	net_move(newBrush + Point3d(0, 0, 0.1));
-	// imshow
+	cout << "Waiting for new brush" << endl;
 	waitKey(0);
 	net_move(newBrush);
 	moveGripper(0.000);
@@ -185,7 +234,7 @@ void net_move(Point3d p) {
 	EcOrientation orientation;
 	orientation.setFrom123Euler(0, 0, 0);
 	pose.setOrientation(orientation);
-	std::cout << "Desired pose:  x: " << pose.translation().x() << " y: " << pose.translation().y() << " z: " << pose.translation().z() << "...";
+	//std::cout << "Desired pose:  x: " << pose.translation().x() << " y: " << pose.translation().y() << " z: " << pose.translation().z() << "...";
 
 	setEndEffectorSet(FRAME_EE_SET); // point end effector set index
 	EcEndEffectorPlacement desiredPlacement(pose);
@@ -218,7 +267,9 @@ void net_move(Point3d p) {
 		}
 
 	}
-	std::cout << (achieved ? "Achieved Pose" : "Failed to Achieve Pose") << std::endl;
+	//std::cout << (achieved ? "Achieved Pose" : "Failed to Achieve Pose") << std::endl;
+	if (!achieved)
+		std::cout << "Failed to Achieve Pose" << std::endl;
 }
 
 bool moveGripper(const double gripperPos) {
---
+++ visualStudioCyton/CytonCV/ImageToStroke.h
@@ -0,0 +1,118 @@
+
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui.hpp"
+#include <assert.h>
+
+using namespace std;
+using namespace cv;
+
+pair<Mat, Mat> discoverColors(Mat image, int numColors) {
+	Mat src = image;
+	Mat samples(src.rows * src.cols, 3, CV_32F);
+	for (int y = 0; y < src.rows; y++)
+		for (int x = 0; x < src.cols; x++)
+			for (int z = 0; z < 3; z++)
+				samples.at<float>(y + x*src.rows, z) = src.at<Vec3b>(y, x)[z];
+
+	Mat labels;
+	int attempts = 5;
+	Mat centers;
+	kmeans(samples, numColors, labels,
+		TermCriteria(TermCriteria::COUNT + TermCriteria::EPS, 1000, 0.001),
+		attempts, KMEANS_PP_CENTERS, centers);
+
+	Mat new_image(src.size(), src.type());
+	for (int y = 0; y < src.rows; y++)
+		for (int x = 0; x < src.cols; x++) {
+			int cluster_idx = labels.at<int>(y + x*src.rows, 0);
+			new_image.at<Vec3b>(y, x)[0] = (uchar)centers.at<float>(cluster_idx, 0);
+			new_image.at<Vec3b>(y, x)[1] = (uchar)centers.at<float>(cluster_idx, 1);
+			new_image.at<Vec3b>(y, x)[2] = (uchar)centers.at<float>(cluster_idx, 2);
+		}
+
+	imshow("clustered image", new_image);
+	return make_pair(centers, labels);
+}
+
+vector<pair<vector<Point>, int> > imageToStroke(Mat image) {
+
+	vector<pair<vector<Point>, int> > output;
+
+	vector<vector<Point> > paths;
+	vector<int> color_indicies;
+
+	//Convert to greyscale, and do a blur to reduce noise.
+	Mat greyscale, blurred;
+	cvtColor(image, greyscale, COLOR_RGB2GRAY);
+	blur(greyscale, blurred, Size((int)(greyscale.cols / 200.0), (int)(greyscale.cols / 200.0)));
+
+	//Get the Edges, magnitude and direction
+	Mat edgeMagnitude, edgeDirection, grad_x, grad_y;
+	Scharr(blurred, grad_x, CV_32F, 1, 0, 3);
+	Scharr(blurred, grad_y, CV_32F, 0, 1, 3);
+	cartToPolar(grad_x, grad_y, edgeMagnitude, edgeDirection);
+
+	//Display the edges 
+	//*
+	vector<Mat> channels;
+	Mat value = Mat::ones(edgeMagnitude.size(), CV_32F);
+	channels.push_back((edgeDirection / CV_2PI) * 360);
+	channels.push_back(value);
+	double max;
+	minMaxIdx(edgeMagnitude, 0, &max);
+	channels.push_back(edgeMagnitude / max);
+	Mat edgeImage;
+	merge(channels, edgeImage);
+	cvtColor(edgeImage, edgeImage, COLOR_HSV2RGB);
+	imshow("edges", edgeImage);
+	//*/
+
+
+	//Discover the colors we'll be using
+	pair<Mat, Mat> colors_labels = discoverColors(image, 4);
+	Mat colors = colors_labels.first;
+	//Also store which color each pixel is nearest to, to save time
+	//when we need to figure out what color to paint an area
+	Mat labels = colors_labels.second;
+	labels = labels.reshape(0, image.size().width);
+
+	//segment the iamge into small areas
+	const float n = 3;
+	//Loop through n*n cells in image (cells may have varying dimensions)
+	for (int i = 0; i <= n + 1; i++) {
+		int colStart = i*floor(greyscale.cols / n);
+		int colEnd = ((i + 1)*floor(greyscale.cols / n)) > greyscale.cols ? greyscale.cols : (i + 1)*floor(greyscale.cols / n);
+		for (int j = 0; j <= n + 1; j++) {
+			int rowStart = j*floor(greyscale.rows / n);
+			int rowEnd = ((j + 1)*floor(greyscale.rows / n)) > greyscale.rows ? greyscale.rows : ((j + 1)*floor(greyscale.rows / n));
+			//Loop through pixels in the cell
+			int numPixels = (rowEnd - rowStart) * (colEnd - colStart);
+			float avgDir = 0;
+			vector<int> colorPop(colors.size().height);
+			for (int col = colStart; col < colEnd; col++) {
+				for (int row = rowStart; row < rowEnd; row++) {
+					avgDir += edgeDirection.at<float>(row, col) * edgeMagnitude.at<float>(row, col) / numPixels;
+					colorPop[labels.at<float>(row, col)] += 1;
+				}
+			}
+			int colorIndex = 0;
+			//Find which color was most popular in that cell
+			for (int k = 0; k < colorPop.size(); k++) {
+				if (colorPop[k] >= colorPop[colorIndex]) {
+					colorIndex = k;
+				}
+			}
+
+			//Add the line with the correct color and direction to output
+			vector<Point> tmp;
+			double lineLength = 0.01;
+			tmp.push_back(Point((i - n / 2) / n + lineLength*cos(avgDir), (j - n / 2) / n + lineLength*sin(avgDir)));
+			tmp.push_back(Point((i - n / 2) / n, (j - n / 2) / n));
+			output.push_back(make_pair(tmp, colorIndex));
+
+		}
+	}
+
+
+	return output;
+}
