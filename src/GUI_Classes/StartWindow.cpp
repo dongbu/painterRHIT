4cd660d8a3690586920b8e7c0c103a53a8a214a8
Zach Dougherty <doughezj@rose-hulman.edu>
2015-08-11 10:57:35 -0400

    added more options in start window

--- src/GUI_Classes/StartWindow.cpp
+++ src/GUI_Classes/StartWindow.cpp
@@ -3,12 +3,14 @@
 #include "Painter.h"
 
 StartWindow::StartWindow(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::StartWindow)
+QWidget(parent),
+ui(new Ui::StartWindow)
 {
-    ui->setupUi(this);
+	ui->setupUi(this);
 
 	maxLength = 800;
+
+	ui->sketchRadio->setChecked(true);
 
 	//all connections for start window
 	connect(ui->width, SIGNAL(editingFinished()), this, SLOT(updateExample()));
@@ -18,18 +20,33 @@ StartWindow::StartWindow(QWidget *parent) :
 	connect(ui->cameraRadio, SIGNAL(clicked()), this, SLOT(radioChanged()));
 	connect(ui->imageRadio, SIGNAL(clicked()), this, SLOT(radioChanged()));
 	connect(ui->sketchRadio, SIGNAL(clicked()), this, SLOT(radioChanged()));
+	connect(ui->cannyCheck, SIGNAL(clicked()), this, SLOT(checkboxChanged()));
+	connect(ui->kMeanCheck, SIGNAL(clicked()), this, SLOT(checkboxChanged()));
 
 	//hiding things as appropriate
 	ui->kMeanCheck->hide();
 	ui->cannyCheck->hide();
-	ui->cameraRadio->setChecked(true);
+	ui->CCEdit->hide();
+	ui->CCLabel->hide();
+	ui->MLLEdit->hide();
+	ui->MLLLabel->hide();
+	ui->TEdit->hide();
+	ui->TLabel->hide();
+	ui->MRSEdit->hide();
+	ui->MRSLabel->hide();
+
+	//force things to be appropriate
 	ui->width->setValidator(new QIntValidator(10, maxLength, this));
 	ui->height->setValidator(new QIntValidator(10, maxLength, this));
+	ui->CCEdit->setValidator(new QIntValidator(1, 64, this));
+	ui->MLLEdit->setValidator(new QIntValidator(1, 100, this));
+	ui->TEdit->setValidator(new QIntValidator(1, 100, this));
+	ui->MRSEdit->setValidator(new QIntValidator(1, 100, this));
 }
 
 StartWindow::~StartWindow()
 {
-    delete ui;
+	delete ui;
 }
 
 
@@ -37,13 +54,13 @@ void StartWindow::updateExample(){
 	int w = 10;
 	int h = 10;
 	if (ui->width->text() != ""){
-		w = (ui->width->text().toInt()/4);
+		w = (ui->width->text().toInt() / 4);
 		if (w < 10){
 			w = 10;
 		}
 	}
 	if (ui->height->text() != ""){
-		h = (ui->height->text().toInt()/4);
+		h = (ui->height->text().toInt() / 4);
 		if (h < 10){
 			h = 10;
 		}
@@ -61,7 +78,6 @@ void StartWindow::loadClicked(){
 }
 void StartWindow::newClicked(){
 	if (ui->cameraRadio->isChecked()){
-		printf("do something here\n");
 		Painter *painter = new Painter();
 		int w = 10;
 		int h = 10;
@@ -79,7 +95,7 @@ void StartWindow::newClicked(){
 		}
 		painter->setDimensions(w, h);
 		painter->showGUI(true);
-		painter->sketch->calibrateWebcam();
+		painter->sketch->loadWebcamPicture();
 		this->close();
 	}
 	else if (ui->sketchRadio->isChecked()){
@@ -147,14 +163,38 @@ void StartWindow::newClicked(){
 }
 void StartWindow::radioChanged(){
 	if (ui->cameraRadio->isChecked()){
-		ui->kMeanCheck->hide();
-		ui->cannyCheck->hide();
+		if (ui->kMeanCheck->isChecked()){
+			ui->CCEdit->show();
+			ui->CCLabel->show();
+			ui->MRSEdit->show();
+			ui->MRSLabel->show();
+		}
+		if (ui->cannyCheck->isChecked()){
+			ui->TEdit->show();
+			ui->TLabel->show();
+			ui->MLLEdit->show();
+			ui->MLLLabel->show();
+		}
+
+		ui->example->show();
+		ui->kMeanCheck->show();
+		ui->cannyCheck->show();
 		ui->aspectRatioLabel->show();
 		ui->X->show();
 		ui->height->show();
 		ui->width->show();
 	}
 	else if (ui->sketchRadio->isChecked()){
+		ui->CCEdit->hide();
+		ui->CCLabel->hide();
+		ui->MLLEdit->hide();
+		ui->MLLLabel->hide();
+		ui->TEdit->hide();
+		ui->TLabel->hide();
+		ui->MRSLabel->hide();
+		ui->MRSEdit->hide();
+
+		ui->example->show();
 		ui->kMeanCheck->hide();
 		ui->cannyCheck->hide();
 		ui->aspectRatioLabel->show();
@@ -163,6 +203,20 @@ void StartWindow::radioChanged(){
 		ui->width->show();
 	}
 	else if (ui->imageRadio->isChecked()){
+		if (ui->kMeanCheck->isChecked()){
+			ui->CCEdit->show();
+			ui->CCLabel->show();
+			ui->MRSEdit->show();
+			ui->MRSLabel->show();
+		}
+		if (ui->cannyCheck->isChecked()){
+			ui->TEdit->show();
+			ui->TLabel->show();
+			ui->MLLEdit->show();
+			ui->MLLLabel->show();
+		}
+
+		ui->example->hide();
 		ui->kMeanCheck->show();
 		ui->cannyCheck->show();
 		ui->aspectRatioLabel->hide();
@@ -171,5 +225,33 @@ void StartWindow::radioChanged(){
 		ui->width->hide();
 
 	}
-	
+
+}
+
+void StartWindow::checkboxChanged(){
+	if (ui->kMeanCheck->isChecked()){
+		ui->CCEdit->show();
+		ui->CCLabel->show();
+		ui->MRSEdit->show();
+		ui->MRSLabel->show();
+	}
+	else{
+		ui->CCEdit->hide();
+		ui->CCLabel->hide();
+		ui->MRSEdit->hide();
+		ui->MRSLabel->hide();
+
+	}
+	if (ui->cannyCheck->isChecked()){
+		ui->TEdit->show();
+		ui->TLabel->show();
+		ui->MLLEdit->show();
+		ui->MLLLabel->show();
+	}
+	else{
+		ui->TEdit->hide();
+		ui->TLabel->hide();
+		ui->MLLEdit->hide();
+		ui->MLLLabel->hide();
+	}
 }
